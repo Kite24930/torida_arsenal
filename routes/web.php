fbf5e8e3ff993c37606e41351948916682c5dbb7
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-11-15 16:24:52 +0900

    background-image fix

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [MainController::class, 'index'])->name('home');
-Route::get('/qrcode', [MainController::class, 'qrcode'])->name('qrcode');
+Route::get('qrcode', [MainController::class, 'qrcode'])->name('qrcode');
