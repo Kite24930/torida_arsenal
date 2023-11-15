bf994b07bde0754378f72fd69d4bbbaab6fea6cc
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-11-15 18:46:58 +0900

    favicon icon fix

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -3,8 +3,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="/favicon.ico">
-    <link rel="apple-touch-icon" href="/icon.png">
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
+    <link rel="apple-touch-icon" href="{{ asset('icon.png') }}">
     {{ $slot }}
     <title>{{ $title }}</title>
 </head>
