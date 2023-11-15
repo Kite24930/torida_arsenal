bcea50adfbddfd2e5e55f041783ce9d8e1c21232
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-11-15 16:26:01 +0900

    route fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,7 +2,7 @@
     <x-header></x-header>
     <main class="min-h-[calc(100dvh-60px)] flex justify-start items-center">
         <div class="w-1/2 flex flex-col items-center justify-center">
-            <a href="/qrcode" class="en-text text-3xl underline">〜 QR TEST 〜</a>
+            <a href="{{ route('qrcode') }}" class="en-text text-3xl underline">〜 QR TEST 〜</a>
         </div>
     </main>
     <x-footer></x-footer>
