a994e722f622b4c1a5953a5ad62d080fea230f58
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-11-20 09:39:12 +0900

    inertia test 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,8 +1,9 @@
 <x-template title="KT Arsenals" css="index.css">
     <x-header></x-header>
     <main class="min-h-[calc(100dvh-60px)] flex justify-start items-center">
-        <div class="w-1/2 flex flex-col items-center justify-center">
+        <div class="w-1/2 flex flex-col items-center justify-center gap-6">
             <a href="{{ route('qrcode') }}" class="en-text text-3xl underline">〜 QR TEST 〜</a>
+            <a href="{{ route('home').'/inertia' }}" class="en-text text-3xl underline">〜 inertia TEST 〜</a>
         </div>
     </main>
     <x-footer></x-footer>
