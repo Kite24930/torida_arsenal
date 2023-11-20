82dff7235cc6ac6a0c0b01f707b0641a90f8c4a6
Kite24930 <71049198+Kite24930@users.noreply.github.com>
2023-11-20 21:13:29 +0900

    inertia test 1

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,7 @@
         <div class="w-1/2 flex flex-col items-center justify-center gap-6">
             <a href="{{ route('qrcode') }}" class="en-text text-3xl underline">〜 QR TEST 〜</a>
             <a href="{{ route('home').'/inertia' }}" class="en-text text-3xl underline">〜 inertia TEST 〜</a>
+            <a href="{{ route('home').'/example' }}" class="en-text text-3xl underline">〜 Example TEST 〜</a>
         </div>
     </main>
     <x-footer></x-footer>
